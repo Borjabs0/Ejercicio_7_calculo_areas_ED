4c2271ad60767609e0962b846865bec87d97883c
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2024-03-15 11:09:04 +0100

    Eliminacion de los atributos x y y en la clase Figuara

--- Figura.java
+++ Figura.java
@@ -1,13 +1,5 @@
 package figuras;
 
 public abstract class Figura {
-    int x;
-    int y;
     public abstract void CalcularArea();
-    int GetX() {
-        return x;
-    }
-    int GetY() {
-        return y;
-    }
 }
